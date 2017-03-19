07de965f08994e14934031fe777ace6ee0d299df
Divine <divine@auroraen.com>
2017-03-18 18:18:11 -0700

    Ticket management endpoints

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const fs = require('fs')
 
+const async = require('async');
+const pg = require('pg');
+
 const PDFDocument = require('pdfkit')
 const BlobStream = require('blob-stream')
 
 const QRCode = require('qrcode')
 
 const app = express()
+
+var config = {
+  user: 'nwhacks',
+  host: '35.161.247.157',
+  database: 'event',
+  port: 26257
+};
+
+var randomString = function(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
 
 app.engine('.hbs', exphbs({  
   defaultLayout: 'main',
@@ -19,6 +38,7 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs')  
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (request, response) => {  
   response.render('home', {
@@ -31,7 +51,7 @@ app.get('/get-ticket', (req, res) => {
 		size: [612.00,310.00]
 	})
 
-	var img = QRCode.toDataURL('1234', (err, url) => {
+	var img = QRCode.toDataURL('CODElol your ticket sucks', (err, url) => {
 		doc.fontSize(25).text('Name: John Smith', 100, 80);
 		doc.image(url)
 		doc.end()
@@ -45,14 +65,57 @@ app.get('/get-ticket', (req, res) => {
 	
 	//doc.image(img)
 
-
-	
-	
-
-
-
 })
 
+app.get('/scan-ticket', (req, res) => {
+	var id = req.query.code
+
+	console.log(id)
+	if (!id){
+		return res.json({'response':0,
+						 'msg': "No ticket ID entered."})
+	} else {
+		pg.connect(config, function (err, client, done) {
+
+
+	  console.log("connecting")
+	  var finish = function () {
+	    done();
+	  };
+
+	  if (err) {
+	    console.error('could not connect to cockroachdb', err);
+	    finish();
+	  }
+
+	  var query = client.query('SELECT * FROM tickets WHERE code=\'' + id +'\'')
+
+
+
+	  query.on('end', (result) => {
+	  	ticket = result.rows[0];
+	  	finish();
+	  	
+		var results = {};
+		if(ticket) {
+		  	if(ticket.used){
+		  		results.response = 0
+		  		results.msg = "Ticket has already been used."
+		  	} else {
+		  		results.response = 1
+		  		results.msg = "Ticket has been successfully registered."
+
+		  	}
+		  } else {
+		  	results.response = 0
+		  	results.msg = "Ticket does not exist."
+		  }
+
+	      return res.json(results);
+	    }
+      );
+	  
+	})}})
 
 
 app.listen(3000)
